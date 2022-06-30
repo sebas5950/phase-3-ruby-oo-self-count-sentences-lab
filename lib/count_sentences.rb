c8a93b2dbeb5c55ebc2716a296ad3ddeaa5c02b6
Alex Guanipatin <sebas5950@gmail.com>
2022-06-29 20:32:37 -0400

    commit tree ede02321d019323261f011de342d96dfd6415516

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,38 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.chars[-1] == "."
+      return true
+    else 
+      false
+    end
   end
 
   def question?
-
-  end
-
+      if self.chars[-1] == "?"
+        return true
+      else 
+        false
+      end
+    end
+ 
   def exclamation?
-
+    if self.chars[-1] == "!"
+      return true
+    else 
+      false
+    end
   end
 
   def count_sentences
-
+    count = 0
+    self.each_char do |char|
+      if char == "!" || char == "?" || char == "." || char == "..."
+        count += 1
+      end
+    end
+    return count
   end
+  binding.pry
 end
+
